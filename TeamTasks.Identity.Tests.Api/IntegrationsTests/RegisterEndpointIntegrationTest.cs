6c2e1ef5811ef67bcb4ce461afec53b4e016e0f7
14LaveN <ponomareff.55555@gmail.com>
2024-07-05 22:49:26 +0600

    Modify the docker-compose file.

---
+++ TeamTasks.Identity.Tests.Api/IntegrationsTests/RegisterEndpointIntegrationTest.cs
@@ -0,0 +1,11 @@
+using TeamTasks.Common.Tests.Abstractions;
+
+namespace TeamTasks.Identity.Tests.Api.IntegrationsTests;
+
+/// <summary>
+/// Represents the register endpoint <see cref="BaseIntegrationTest"/>
+/// </summary>
+public sealed class RegisterEndpointIntegrationTest
+{
+    
+}
